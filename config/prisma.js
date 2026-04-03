6610bfe4e587b4acf629b648ab05da49fea513b0
FinCore Dev <fincore.dev@gmail.com>
2026-04-03 13:15:00 +0000

    feat: set up Prisma client singleton with pg driver adapter

---
+++ config/prisma.js
@@ -0,0 +1,11 @@
+const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { Pool } = require("pg");
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+module.exports = prisma;
